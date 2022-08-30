64b051c5a74c2bb2293762dd18ec9eb2eb26ca59
Yoba Fett <alexka.680@gmail.com>
2022-08-30 19:50:33 +0900

    Insert new components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import nextId from "react-id-generator";
 
 import HeroBlock from '../hero-block/hero-block';
+import AboutUs from '../about-us/about-us';
 
 import './app.sass';
 
@@ -24,16 +25,28 @@ class App extends Component {
                     onClick: '',
                     id: nextId(),
                 },
-            ]
+            ],
+            aboutUsText: `Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+            Afraid at highly months do things on at. Situation recommend objection do intention
+            so questions. As greatly removed calling pleased improve an. Last ask him cold feel
+            met spot shy want. Children me laughing we prospect answered followed. At it went
+            is song that held help face.
+            
+            Now residence dashwoods she excellent you. Shade being under his bed her, Much
+            read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
+            horrible but confined day end marriage. Eagerness furniture set preserved far
+            recommend. Did even but nor are most gave hope. Secure active living depend son
+            repair day ladies now.`,
         }
     }
 
     render() {
-        const { navLogo, navItems } = this.state;
+        const { navLogo, navItems, aboutUsText } = this.state;
 
         return (
             <div>
                 <HeroBlock navLogo={navLogo} navItems={navItems} />
+                <AboutUs aboutUsText={aboutUsText} />
             </div>
         );
     }
