05aea67524ecfa33186f03f4cd448703b6d742a4
Yoba Fett <alexka.680@gmail.com>
2022-08-31 21:38:22 +0900

    Add about beans component

---
+++ src/components/about-beans/about-beans.js
@@ -0,0 +1,27 @@
+import AboutUs from '../about-us/about-us';
+
+import './about-beans.sass';
+
+function AboutBeans(props) {
+    const aboutUsText = `
+    Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+
+    Afraid at highly months do things on at. Situation recommend objection do intention
+    so questions. 
+    As greatly removed calling pleased improve an. Last ask him cold feel
+    met spot shy want. Children me laughing we prospect answered followed. At it went
+    is song that held help face.`;
+    const img = require(`../../assets/img/girl-865304_1920.jpg`);
+
+    return (
+        <div className="about-beans">
+            <div className="wrapper">
+                <img className="about-pic" src={img} alt="" />
+                <AboutUs aboutUsText={aboutUsText} />
+            </div>
+            <div className="simple-separator"></div>
+        </div>
+    );
+}
+
+export default AboutBeans;
