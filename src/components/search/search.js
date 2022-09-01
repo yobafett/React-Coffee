dbe518380583c3631595d2eb4b3c1ce15d86e93e
Yoba Fett <alexka.680@gmail.com>
2022-09-01 20:46:30 +0900

    Add search component

---
+++ src/components/search/search.js
@@ -0,0 +1,15 @@
+
+import './search.sass';
+
+function Search(props) {
+    const { label,placeholder, onSearchChange } = props;
+
+    return (
+        <div className="search" >
+            <span>{label}</span>
+            <input type="text" onChange={onSearchChange} placeholder={placeholder} />
+        </div>
+    );
+}
+
+export default Search;
