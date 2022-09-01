225e129f7a447ae8edcf33e6717e9dd6e843882d
Yoba Fett <alexka.680@gmail.com>
2022-09-02 08:16:40 +0900

    Add header block customise

--- src/components/coffee-page/coffee-page.js
+++ src/components/coffee-page/coffee-page.js
@@ -4,13 +4,19 @@ import AboutBeans from '../about-beans/about-beans';
 import GoodsBlock from '../goods-block/goods-block';
 import Footer from '../footer/footer';
 
+import HeaderBg from '../../assets/img/coffee-shop-1209863_1920.jpg';
+
 import './coffee-page.sass';
 
 function CoffeePage(props) {
     const { navLogo, navItems, coffeeItems, onSearchChange, onFilterChange, filters, currentFilter } = props;
     return (
         <div className='coffee-page'>
-            <HeaderBlock navLogo={navLogo} navItems={navItems} />
+            <HeaderBlock
+                navLogo={navLogo}
+                navItems={navItems}
+                background={HeaderBg}
+                h1Title='Our Coffee' />
             <AboutBeans />
             <GoodsBlock
                 coffeeItems={coffeeItems}
--- src/components/header-block/header-block.js
+++ src/components/header-block/header-block.js
@@ -4,10 +4,12 @@ import H1 from '../h1/h1';
 import './header-block.sass';
 
 function HeaderBlock(props) {
+    const { h1Title, background, navLogo, navItems } = props;
+
     return (
-        <div className="header-block">
-            <NavBar navLogo={props.navLogo} navItems={props.navItems} color={'white'} />
-            <H1 text={'Our Coffee'} color={'#fff'} shadow={false} />
+        <div className="header-block" style={{ backgroundImage: `url(${background})` }}>
+            <NavBar navLogo={navLogo} navItems={navItems} color={'white'} />
+            <H1 text={h1Title} color={'#fff'} shadow={false} />
         </div>
     );
 }
