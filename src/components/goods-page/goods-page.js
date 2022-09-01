f0e7d3df6fafabc97aba6e343aafe6b8ada35817
Yoba Fett <alexka.680@gmail.com>
2022-09-02 08:17:08 +0900

    Goods page component - WIP

---
+++ src/components/goods-page/goods-page.js
@@ -0,0 +1,32 @@
+
+import HeaderBlock from '../header-block/header-block';
+import AboutBeans from '../about-beans/about-beans';
+import GoodsBlock from '../goods-block/goods-block';
+import Footer from '../footer/footer';
+import HeaderBg from '../../assets/img/coffee-2179009_1920.jpg';
+
+import './goods-page.sass';
+
+function GoodsPage(props) {
+    const { navLogo, navItems, coffeeItems, onSearchChange, onFilterChange, filters, currentFilter } = props;
+
+    return (
+        <div className='goods-page'>
+            <HeaderBlock
+                navLogo={navLogo}
+                navItems={navItems}
+                background={HeaderBg}
+                h1Title='For your pleasure' />
+            <AboutBeans />
+            <GoodsBlock
+                coffeeItems={coffeeItems}
+                onSearchChange={onSearchChange}
+                onFilterChange={onFilterChange}
+                currentFilter={currentFilter}
+                filters={filters} />
+            <Footer navLogo={navLogo} navItems={navItems} color={'black'} />
+        </div>
+    );
+}
+
+export default GoodsPage;
