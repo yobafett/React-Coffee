0bc9cdef323e963f4c746a750e5fb04999af39f9
Yoba Fett <alexka.680@gmail.com>
2022-09-02 21:02:59 +0900

    Add AboutPic params

--- src/components/goods-page/goods-page.js
+++ src/components/goods-page/goods-page.js
@@ -10,6 +10,16 @@ import './goods-page.sass';
 function GoodsPage(props) {
     const { navLogo, navItems, coffeeItems, onSearchChange, onFilterChange, filters, currentFilter } = props;
 
+    const aboutUsTitle = 'About our goods';
+    const aboutUsText = `Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+
+    Afraid at highly months do things on at. Situation recommend objection do intention
+    so questions. 
+    As greatly removed calling pleased improve an. Last ask him cold feel
+    met spot shy want. Children me laughing we prospect answered followed. At it went
+    is song that held help face.`;
+    const aboutImg = require(`../../assets/img/Mask Group.jpg`);
+
     return (
         <div className='goods-page'>
             <HeaderBlock
@@ -17,7 +27,10 @@ function GoodsPage(props) {
                 navItems={navItems}
                 background={HeaderBg}
                 h1Title='For your pleasure' />
-            <AboutPic />
+            <AboutPic
+                aboutUsTitle={aboutUsTitle}
+                aboutUsText={aboutUsText}
+                aboutImg={aboutImg} />
             <GoodsBlock
                 coffeeItems={coffeeItems}
                 onSearchChange={onSearchChange}
