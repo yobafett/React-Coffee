9efb5cdf74546abc4c166d895b9af8de096c0f1f
Yoba Fett <alexka.680@gmail.com>
2022-09-02 21:14:41 +0900

    Add underH1 block

--- src/components/hero-block/hero-block.js
+++ src/components/hero-block/hero-block.js
@@ -5,11 +5,24 @@ import Separator from '../separator/separator';
 import './hero-block.sass';
 
 function HeroBlock(props) {
+    const h1Title = 'Everything You Love About Coffee';
+    const textStrings = [
+        'We makes every day full of energy and taste',
+        'Want to try our beans?'
+    ];
+    const btnText = 'More';
+
+    const spans = textStrings.map(el => (<span>{el}</span>));
+
     return (
         <div className="hero-block">
             <NavBar navLogo={props.navLogo} navItems={props.navItems} color={'white'} />
-            <H1 text={'Everything You Love About Coffee'} color={'#fff'} shadow={true} />
+            <H1 text={h1Title} color={'#fff'} shadow={true} />
             <Separator color={'white'} />
+            <div className='underH1'>
+                {spans}
+                <button>{btnText}</button>
+            </div>
         </div>
     );
 }
