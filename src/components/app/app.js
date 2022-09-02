ca8172c84177bc7953bf14f3191bcac956ce0bf0
Yoba Fett <alexka.680@gmail.com>
2022-09-02 21:01:26 +0900

    Move aboutUsText from state of app to main-pg comp

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -34,17 +34,6 @@ class App extends Component {
                     }
                 },
             ],
-            aboutUsText: `Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-            Afraid at highly months do things on at. Situation recommend objection do intention
-            so questions. As greatly removed calling pleased improve an. Last ask him cold feel
-            met spot shy want. Children me laughing we prospect answered followed. At it went
-            is song that held help face.
-            
-            Now residence dashwoods she excellent you. Shade being under his bed her, Much
-            read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
-            horrible but confined day end marriage. Eagerness furniture set preserved far
-            recommend. Did even but nor are most gave hope. Secure active living depend son
-            repair day ladies now.`,
             coffeeItems: [
                 {
                     id: nextId(),
@@ -166,7 +155,7 @@ class App extends Component {
     }
 
     render() {
-        const { navLogo, navItems, aboutUsText, coffeeItems, currentPage, filters, currentFilter } = this.state;
+        const { navLogo, navItems, coffeeItems, currentPage, filters, currentFilter } = this.state;
 
         let page;
         switch (currentPage) {
@@ -211,7 +200,6 @@ class App extends Component {
                     <MainPage
                         navLogo={navLogo}
                         navItems={navItems}
-                        aboutUsText={aboutUsText}
                         coffeeItems={coffeeItems.filter(item => item.isBest)} />
                 );
                 break;
--- src/components/main-page/main-page.js
+++ src/components/main-page/main-page.js
@@ -6,11 +6,25 @@ import Footer from '../footer/footer';
 import './main-page.sass';
 
 function MainPage(props) {
-    const { navLogo, navItems, aboutUsText, coffeeItems } = props;
+    const { navLogo, navItems, coffeeItems } = props;
+
+    const aboutUsTitle = 'About Us';
+    const aboutUsText = `Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+            Afraid at highly months do things on at. Situation recommend objection do intention
+            so questions. As greatly removed calling pleased improve an. Last ask him cold feel
+            met spot shy want. Children me laughing we prospect answered followed. At it went
+            is song that held help face.
+            
+            Now residence dashwoods she excellent you. Shade being under his bed her, Much
+            read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
+            horrible but confined day end marriage. Eagerness furniture set preserved far
+            recommend. Did even but nor are most gave hope. Secure active living depend son
+            repair day ladies now.`;
+
     return (
         <div className='main-page'>
             <HeroBlock navLogo={navLogo} navItems={navItems} />
-            <AboutUs aboutUsText={aboutUsText} />
+            <AboutUs aboutUsTitle={aboutUsTitle} aboutUsText={aboutUsText} />
             <OurBestBlock coffeeItems={coffeeItems} />
             <Footer navLogo={navLogo} navItems={navItems} color={'black'} />
         </div>
