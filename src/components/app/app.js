9779af13d602a3aefb8cf203696e317cfbc669c3
Yoba Fett <alexka.680@gmail.com>
2022-09-02 20:48:28 +0900

    TMP - Set defaul page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,7 +11,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentPage: 'coffee',
+            currentPage: 'pleasure',
             navLogo: {
                 title: 'Coffee house',
                 onClick: () => {
