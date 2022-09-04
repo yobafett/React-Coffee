09daef6aca7fdb85be9240a83189d09a162d26d7
Yoba Fett <alexka.680@gmail.com>
2022-09-04 17:05:49 +0900

    Refactoring - Hero-block

--- src/components/hero-block/hero-block.js
+++ src/components/hero-block/hero-block.js
@@ -5,19 +5,21 @@ import Separator from '../separator/separator';
 import './hero-block.sass';
 
 function HeroBlock(props) {
-    const h1Title = 'Everything You Love About Coffee';
-    const textStrings = [
+    const { navLogo, navItems } = props;
+
+    const title = 'Everything You Love About Coffee';
+    const content = [
         'We makes every day full of energy and taste',
         'Want to try our beans?'
     ];
     const btnText = 'More';
 
-    const spans = textStrings.map((el, i) => (<span key={i}>{el}</span>));
+    const spans = content.map((el, i) => (<span key={i}>{el}</span>));
 
     return (
         <div className="hero-block">
-            <NavBar navLogo={props.navLogo} navItems={props.navItems} color={'white'} />
-            <H1 content={h1Title} color={'#fff'} shadow={true} />
+            <NavBar navLogo={navLogo} navItems={navItems} color={'white'} />
+            <H1 content={title} color={'white'} shadow={true} />
             <Separator color={'white'} />
             <div className='underH1'>
                 {spans}
